8a89d866cafe3cc381d084bcb42df9dcd67ec183
Jesse Onolememen <hey@jesseono.me>
2021-08-01 12:00:29 +0100

    chore: init backend

---
+++ backend/config/application.rb
@@ -0,0 +1,30 @@
+require_relative 'boot'
+
+require 'rails'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Backend
+  class Application < Rails::Application
+    config.load_defaults 6.1
+    config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CacheStore
+    config.middleware.use Rack::MethodOverride
+
+    config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+    config.session_store :cache_store
+  end
+end
