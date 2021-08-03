a31f498ce5d9a5cfc76767536fbd3261990d5aad
Jesse Onolememen <hey@jesseono.me>
2021-08-03 15:13:53 +0100

    fix eslint errors

--- app/src/shared/views/all.page.tsx
+++ app/src/shared/views/all.page.tsx
@@ -1,7 +1,5 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import { App } from '../app'
-import { theme } from '../theme'
 
 export function Page() {
   return <App />
